bcd0ecaae168c53134d4bcf969b5e98116712459
Matthew Penner <matthewcpenner@gmail.com>
2018-12-21 14:35:17 -0700

    Started work on conversion script

--- convert.py
+++ convert.py
@@ -1,2 +1,10 @@
 import csv
+import argparse
 
+parser = argparse.ArgumentParses()
+parser.addArgument('-s', '--source', default='data.csv')
+parser.addArgument('-d', '--destination', default='clean_data.csv')
+
+if __name__ = '__main__':
+    with open(source, 'r', encoding='utf_16_le'):
+        
